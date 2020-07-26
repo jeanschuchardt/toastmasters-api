af127fbafc40a46cd5e688e9da8f1423fae77782
Jean_Burda <jean_burda@dell.com>
2020-07-26 15:34:50 -0300

    frist commit

---
+++ src/main/java/toastmastersapi/toastmasters/ToastmastersApplication.java
@@ -0,0 +1,13 @@
+package toastmastersapi.toastmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastmastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastmastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toastmastersapi/toastmasters/ToastmastersApplicationTests.java
@@ -0,0 +1,13 @@
+package toastmastersapi.toastmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastmastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
